afc48e72b74eacba6f52e403bd5e93748c07c9e6
Joshua E. Jodesty <joshua.jodesty@gmail.com>
2018-11-13 14:13:00 -0500

    Fill output NANs with previous value

--- engine/run.py
+++ engine/run.py
@@ -3,7 +3,6 @@ from engine.configProcessor import generate_config, create_tensor_field
 from engine.mechanismExecutor import simulation
 from engine.utils import flatten
 from tabulate import tabulate
-#from tabulate import tabulate
 import pandas as pd
 
 def main():
@@ -13,17 +12,13 @@ def main():
     # print(len(configs))
     print(tabulate(create_tensor_field(mechanisms, ep), headers='keys', tablefmt='psql'))
     print
-    # print(configs)
-    # print(states_list)
-    # print(configs)
     # p = pipeline(states_list, configs, env_processes, range(10))
     T = sim_config['T']
     N = sim_config['N']
 
     # Dimensions: N x r x mechs
     s = simulation(states_list, configs, env_processes, T, N)
+
     result = pd.DataFrame(flatten(s))
-    # print('Test')
-#    print(tabulate(result, headers='keys', tablefmt='psql'))
-# remove print and tabulate functions, so it returns a dataframe
+
     return result
