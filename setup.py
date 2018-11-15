86c4edfacd00e5b7f06ab0ea980cb0799c94a44f
Joshua E. Jodesty <joshua.jodesty@gmail.com>
2018-11-15 18:55:49 -0500

    need license

--- setup.py
+++ setup.py
@@ -6,6 +6,6 @@ setup(name='SimCAD',
       url='https://github.com/BlockScience/DiffyQ-SimCAD',
       author='Joshua E. Jodesty',
       author_email='joshua@block.science',
-      # license='MIT',
+      # license='?????',
       packages=['engine'],
       zip_safe=False)
