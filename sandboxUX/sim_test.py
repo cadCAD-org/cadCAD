7ecd4e6c86e35a418fd12a0266edf28a06aaf12c
Joshua E. Jodesty <joshua.jodesty@gmail.com>
2018-11-15 18:48:39 -0500

    Reafactor Pt. 5: Improved Runtime Env / ux Pt.3

--- sandboxUX/sim_test.py
+++ sandboxUX/sim_test.py
@@ -1,7 +1,26 @@
+import pandas as pd
+from tabulate import tabulate
 
 from SimCAD.engine import ExecutionContext, Executor
 from sandboxUX import config1, config2
 
+print("Simulation Run 1")
+print()
+single_config = [config1]
+run1 = Executor(ExecutionContext, single_config)
+run1_raw_result = run1.main()
+result = pd.DataFrame(run1_raw_result)
+print(tabulate(result, headers='keys', tablefmt='psql'))
+print()
+
+print("Simulation Run 2: Pairwise Execution")
+print()
 configs = [config1, config2]
-run = Executor(ExecutionContext, configs)
-result = run.main()
+run2 = Executor(ExecutionContext, configs)
+run2_raw_results = run2.main()
+for result in run2_raw_results:
+    print(tabulate(result, headers='keys', tablefmt='psql'))
+print()
+
+
+
