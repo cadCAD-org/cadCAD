19503e3d325694fa25748e6692aebbce39ee1ade
Joshua E. Jodesty <joshua.jodesty@gmail.com>
2019-01-10 21:38:38 -0500

    resolved ModuleNotFoundError

--- simulations/sim_test.py
+++ simulations/sim_test.py
@@ -1,12 +1,10 @@
 import pandas as pd
 from tabulate import tabulate
 
-# The following imports NEED to be in the exact same order
+# The following imports NEED to be in the exact order
 from SimCAD.engine import ExecutionMode, ExecutionContext, Executor
-from simulations.validation import config_1, config_2
+from simulations.validation import config1, config2
 from SimCAD import configs
-
-# ToDo: pass ExecutionContext with execution method as ExecutionContext input
 
 exec_mode = ExecutionMode()
 
@@ -18,7 +16,6 @@ single_proc_ctx = ExecutionContext(context=exec_mode.single_proc)
 run1 = Executor(exec_context=single_proc_ctx, configs=first_config)
 run1_raw_result, tensor_field = run1.main()
 result = pd.DataFrame(run1_raw_result)
-# result.to_csv('~/Projects/DiffyQ-SimCAD/results/config4.csv', sep=',')
 print()
 print("Tensor Field:")
 print(tabulate(tensor_field, headers='keys', tablefmt='psql'))
