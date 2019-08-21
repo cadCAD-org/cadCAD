f4a5c27421a51739cc4b52339c1322c58c1e6d00
Joshua E. Jodesty <joshua.jodesty@gmail.com>
2019-08-21 14:27:31 -0400

    os refactor pt 1

--- cadCAD/engine/__init__.py
+++ cadCAD/engine/__init__.py
@@ -93,7 +93,6 @@ class Executor:
         final_result = None
 
         if self.exec_context == ExecutionMode.single_proc:
-            # ToDO: Deprication Handler - "sanitize" in appropriate place
             tensor_field = create_tensor_field(partial_state_updates.pop(), eps.pop())
             result = self.exec_method(simulation_execs, var_dict_list, states_lists, configs_structs, env_processes_list, Ts, Ns)
             final_result = result, tensor_field
