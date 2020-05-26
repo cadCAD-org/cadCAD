7b9999e9a0e37af25452d85cef97a15795009143
Benjamin Scholtz <contact@benjaminscholtz.com>
2020-05-26 16:44:55 +0200

    Resolve review comments

--- default.nix
+++ default.nix
@@ -1,11 +1,12 @@
 { pkgs ? import <nixpkgs> { }, stdenv ? pkgs.stdenv, fetchPypi ? pkgs.fetchPypi
-, pythonPkgs ? pkgs.python37Packages
+, pythonPkgs ? pkgs.python36Packages
 , buildPythonPackage ? pythonPkgs.buildPythonPackage }:
 
 buildPythonPackage rec {
   pname = "cadCAD";
-  version = "0.3.1";
+  version = "0.4.15";
 
+  # Nix allows fetching the project source from different locations
   #src = fetchPypi {
   #  inherit pname version;
   #  sha256 = "4f2a4d39e4ea601b9ab42b2db08b5918a9538c168cff1c6895ae26646f3d73b1";
@@ -16,7 +17,8 @@ buildPythonPackage rec {
   # };
   src = ./.;
 
-  doCheck = false; # TODO: correct or disclude integration tests
+  # In future, when tests are introduced, this can be updated to 'true'
+  doCheck = false;
 
   buildInputs = [ ];
   checkInputs = [ ];
