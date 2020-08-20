885a070f0bb8fe23c02313ca227619201d744270
Joshua E. Jodesty <joshua.jodesty@gmail.com>
2020-08-20 16:25:10 -0400

    update build

--- simulations/regression_tests/poc/install_local_kafka.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-wget http://apache.spinellicreations.com/kafka/2.3.0/kafka_2.12-2.3.0.tgz
-tar -xzf kafka_2.12-2.3.0.tgz
-cd kafka_2.12-2.3.0
--- simulations/regression_tests/poc/launch_local_kafka.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-bin/zookeeper-server-start.sh config/zookeeper.properties &
-bin/kafka-server-start.sh config/server.properties &
-bin/kafka-topics.sh --create --bootstrap-server localhost:9092 --replication-factor 1 --partitions 1 --topic test
-bin/kafka-console-consumer.sh --bootstrap-server localhost:9092 --topic test --from-beginning
