b579c13809759aa0e4dd6053122034e16704c34d
JEJodesty <joshua.jodesty@gmail.com>
2021-04-07 13:51:49 -0400

    converted to process pool

--- cadCAD/engine/execution.py
+++ cadCAD/engine/execution.py
@@ -1,5 +1,4 @@
 from typing import Callable, Dict, List, Any, Tuple
-from pathos.multiprocessing import ThreadPool as TPool
 from pathos.multiprocessing import ProcessPool as PPool
 from collections import Counter
 
