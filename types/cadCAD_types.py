597557ec5393c04bd4c853bc8a3259ddf4e9bde4
Danillo Lessa Bernardineli <danilo.lessa@gmail.com>
2021-05-06 15:41:00 -0300

    update types

--- types/cadCAD_types.py
+++ types/cadCAD_types.py
@@ -1,6 +1,16 @@
-from typing import List, Dict
+from typing import Callable, List, Dict, Tuple
 
 StateVariable = object
 Parameter = List[object]
-StateUpdateBlock = Dict[str, Dict[str, callable]]
+
+Params = dict
+State = dict
+Signal = dict
+History = List[List[State]]
+Substep = int
+
+Policy = Callable[[Params, Substep, History, State], dict]
+StateUpdate = Callable[[Params, Substep, History, State, Signal], Tuple[str, object]]
+
+StateUpdateBlock = Dict[str, object]
 TimestepBlock = List[StateUpdateBlock]
