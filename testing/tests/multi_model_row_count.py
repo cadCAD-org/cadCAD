41e843eb1a16761e63aee36c4e20ea0c2de2a492
JEJodesty <joshua.jodesty@gmail.com>
2021-06-11 13:53:49 -0400

    ci: added record count generation from previous version

--- testing/tests/multi_model_row_count.py
+++ testing/tests/multi_model_row_count.py
@@ -1,6 +1,5 @@
 import json
 import os
-import pathlib
 import unittest, pandas as pd
 from cadCAD.configuration import Experiment
 from cadCAD.engine import ExecutionMode, ExecutionContext, Executor
@@ -98,8 +97,6 @@ class RowCountTest(unittest.TestCase):
     def test_row_count_from_sys_model_C(self):
         self.assertEqual(model_C_rows == policy_agg_df_rows, True, f"{sys_model_C_id}: Row Count Mismatch with Expected results")
     def test_a_b_row_count(self):
-        # file_dir = pathlib.Path(__file__).parent.absolute()
-        print(os.getcwd())
         file_path = f'{os.getcwd()}/testing/tests/a_b_tests/0_4_23_record_count.json'
         record_count_0_4_23 = json.load(open(file_path))['record_count']
         record_count_0_4_24 = result_rows
