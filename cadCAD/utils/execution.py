b341e53a20bb1122ddcbd8cd9656f98e7bc64572
JEJodesty <joshua.jodesty@gmail.com>
2021-07-06 15:51:55 -0400

    new build, new print_exec_info

--- cadCAD/utils/execution.py
+++ cadCAD/utils/execution.py
@@ -11,17 +11,18 @@ def print_exec_info(exec_context, configs):
     models = len(configs)
     sim_strs, run_vals, timestep_vals, params, sub_states = [], [], [], [], set()
     for i, config in enumerate(configs):
-        run_vals.append(config.sim_config['N'])
-        for timestep in [*config.sim_config['T']]:
+        sim_config = config.sim_config
+        run_vals.append(sim_config['N'])
+        for timestep in [*sim_config['T']]:
             timestep_vals.append(timestep)
-        if type(config.sim_config['M']) is dict:
-            params.append(len(config.sim_config['M']))
+        if type(sim_config['M']) is dict:
+            params.append(len(sim_config['M']))
         for state_key in list(config.initial_state.keys()):
             sub_states.add(state_key)
-        sim = config.sim_config
-        n_t = len(sim['T'])
-        n_m = len(sim['M'])
-        n_n = sim['N']
+
+        n_t = len(sim_config['T'])
+        n_m = len(sim_config['M'])
+        n_n = sim_config['N']
         n_s = len(config.initial_state)
         sim_strs.append(f'     Simulation {i}: (Timesteps, Params, Runs, Sub-States) = ({n_t}, {n_m}, {n_n}, {n_s})')
 
