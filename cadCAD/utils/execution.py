58d9cfd0b914677d80ab6a1cce79dc8009bf39ee
JEJodesty <joshua.jodesty@gmail.com>
2021-07-06 19:00:09 -0400

    new print_exec_info

--- cadCAD/utils/execution.py
+++ cadCAD/utils/execution.py
@@ -12,25 +12,27 @@ def print_exec_info(exec_context, configs):
     sim_strs, run_vals, timestep_vals, params, sub_states = [], [], [], [], set()
     for i, config in enumerate(configs):
         sim_config = config.sim_config
-        run_vals.append(sim_config['N'])
+        n_n = sim_config['N']
+        n_t = len(sim_config['T'])
+        n_m = len(sim_config['M'])
+        n_s = len(config.initial_state)
+        run_vals.append(n_n)
         for timestep in [*sim_config['T']]:
             timestep_vals.append(timestep)
         if type(sim_config['M']) is dict:
-            params.append(len(sim_config['M']))
+            params.append(n_m)
+        else:
+            n_m = 0
         for state_key in list(config.initial_state.keys()):
             sub_states.add(state_key)
 
-        n_t = len(sim_config['T'])
-        n_m = len(sim_config['M'])
-        n_n = sim_config['N']
-        n_s = len(config.initial_state)
         sim_strs.append(f'     Simulation {i}: (Timesteps, Params, Runs, Sub-States) = ({n_t}, {n_m}, {n_n}, {n_s})')
 
     timesteps = len(timestep_vals)
     if sum(params) != 0:
         param_count = sum(params)
     else:
-        param_count = 1
+        param_count = 0
     runs = sum(run_vals)
     init_states = len(sub_states)
 
