ca1fe549e15079f9e5fdc4fa6390707d6214f158
Danilo Lessa Bernardineli <danilo.lessa@gmail.com>
2023-03-08 16:32:32 -0300

    rm fn dependency

--- setup.py
+++ setup.py
@@ -20,7 +20,6 @@ setup(name=name,
       packages=find_packages(),
       install_requires=[
             "pandas",
-            "fn",
             "funcy",
             "dill",
             "pathos",
