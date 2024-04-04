9fc2ffcf9783305b5ff93414550f1d87917d0890
Danilo Lessa Bernardineli <danilo.lessa@gmail.com>
2024-04-04 09:21:59 -0300

    update tests

--- testing/test_param_count.py
+++ testing/test_param_count.py
@@ -3,6 +3,7 @@ from cadCAD.configuration.utils import config_sim
 from cadCAD.engine import Executor, ExecutionContext, ExecutionMode
 import pytest
 
+
 P_no_lst = {'pA': 1, 'pB': 2, 'pC': 3}
 P_single_lst = {'pA': [1], 'pB': [1], 'pC': [3]}
 P_single_swp = {'pA': [4, 5, 6], 'pB': [1], 'pC': [3]}
@@ -10,9 +11,13 @@ P_all_swp = {'pA': [7, 8, 9], 'pB': [1, 2, 3], 'pC': [1, 2, 3]}
 P_all_but_one_swp = {'pA': [7, 8, 9], 'pB': [1, 2, 3], 'pC': [1]}
 Ps = [P_no_lst, P_single_lst, P_single_swp, P_all_swp, P_all_but_one_swp]
 
-CONFIG_SIGNATURES_TO_TEST = [(3, 3, 3, 3, 3), (1, 3, 3, 3, 3),
- (3, 1, 3, 3, 3), (1, 1, 3, 3, 3),
- (3, 3, 1, 3, 3), (1, 3, 1, 3, 3), (1, 1, 1, 3, 3)]
+CONFIG_SIGNATURES_TO_TEST = [(3, 3, 3, 3, 3),
+                             (1, 3, 3, 3, 3),
+                             (3, 1, 3, 3, 3),
+                             (1, 1, 3, 3, 3),
+                             (3, 3, 1, 3, 3),
+                             (1, 3, 1, 3, 3),
+                             (1, 1, 1, 3, 3)]
 
 
 def run_experiment(exp: Experiment, mode: str):
@@ -35,10 +40,12 @@ def param_count_test_policy_generator(provided_params):
         return {'sigA': None}
     return p_test_param_count
 
+
 def create_experiments(N_simulations=3, N_sweeps=3, N_runs=3, N_timesteps=3, N_substeps=3, params={}) -> Experiment:
 
     INITIAL_STATE = {'varA': None}
-    PSUBs = [{'policies': {'sigA': param_count_test_policy_generator(params)}, 'variables': {'varA': param_count_test_suf_generator(params)}}] * N_substeps
+    PSUBs = [{'policies': {'sigA': param_count_test_policy_generator(
+        params)}, 'variables': {'varA': param_count_test_suf_generator(params)}}] * N_substeps
 
     SIM_CONFIG = config_sim(
         {
@@ -58,8 +65,8 @@ def create_experiments(N_simulations=3, N_sweeps=3, N_runs=3, N_timesteps=3, N_s
     return exp
 
 
-def expected_rows(N_simulations, N_sweeps, N_runs, N_timesteps, N_substeps,P) -> int:
-    return N_simulations * N_sweeps *  N_runs * (N_timesteps * N_substeps + 1)
+def expected_rows(N_simulations, N_sweeps, N_runs, N_timesteps, N_substeps, P) -> int:
+    return N_simulations * N_sweeps * N_runs * (N_timesteps * N_substeps + 1)
 
 
 @pytest.mark.parametrize("N_sim,N_sw,N_r,N_t,N_s", CONFIG_SIGNATURES_TO_TEST)
@@ -67,7 +74,6 @@ def expected_rows(N_simulations, N_sweeps, N_runs, N_timesteps, N_substeps,P) ->
 def test_row_count_single(N_sim, N_sw, N_r, N_t, N_s, P):
     args = (N_sim, N_sw, N_r, N_t, N_s, P)
     len(run_experiment(create_experiments(*args), 'single_proc'))
-
 
 
 @pytest.mark.parametrize("N_sim,N_sw,N_r,N_t,N_s", CONFIG_SIGNATURES_TO_TEST)
